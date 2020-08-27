ec3dab2fc04079f4dd5e6fc02aadac45e7cc44f0
Michael Boyle <michael.oliver.boyle@gmail.com>
2020-08-27 16:11:14 -0400

    Cast to fftw_complex

--- simple.c
+++ simple.c
@@ -16,12 +16,20 @@ int main() {
   /* fftw_complex c[Nm]; */  /* error C2057: expected constant expression */
 
   for (m=0; m<Nm; m++) {
+    c[m] = (fftw_complex){0.0, 0.0};
+
+    c[m] = (fftw_complex)0.0;
+
     c[m] = {0.0, 0.0};  /* error C2059: syntax error: '{' */
 
     c[m][0] = 0.0;  /* works */
     c[m][1] = 0.0;
 
-    c[m] = (fftw_complex){0.0, 0.0};  /* error C2106: '=': left operand must be l-value */
+    /* Obviously, this one errors out differently if complex.h is not included */
+    c[m] = (_Dcomplex){0.0, 0.0};  /* error C2275: '_Dcomplex': illegal use of this type as an expression
+        C:\Program Files (x86)\Windows Kits\10\include\10.0.19041.0\ucrt\complex.h(47): note: see declaration of '_Dcomplex'
+        simple.c(56): error C2143: syntax error: missing ';' before '{'
+        simple.c(56): error C2143: syntax error: missing ';' before '}' */
 
     /* Obviously, this one errors out differently if complex.h is not included */
     c[m] = _Dcomplex{0.0, 0.0};  /* error C2275: '_Dcomplex': illegal use of this type as an expression
