cfa5f9a9f655c602dfdb39d61cd66b72b198ffc7
Michael Boyle <michael.oliver.boyle@gmail.com>
2020-08-27 10:03:17 -0400

    Remove MSVC complex

--- simple.c
+++ simple.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <math.h>
-#include <complex.h>
-#include "config.h"
+/* #include <complex.h> */
+/* #include "config.h" */
+#define FFTW_NO_Complex  /* with this, fftw just does `typedef double fftw_complex[2]` */
 #include "fftw3.h"
 
 int main() {
@@ -12,7 +13,7 @@ int main() {
   fftw_complex *c = fftw_malloc(Nm*sizeof(fftw_complex));
 
   for (m=0; m<Nm; m++) {
-    c[m] = (m+1.)+(3.*m-1.)*I;
+    c[m] = {0.0, 0.0};
   }
 
   printf("Made it to the end.\n");
