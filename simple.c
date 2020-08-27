9d64f7f587c7d2a52ec23c3fd3f7ba7901eb1790
Michael Boyle <michael.oliver.boyle@gmail.com>
2020-08-26 23:00:00 -0400

    Add some fftw stuff

--- simple.c
+++ simple.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
+#include "fftw3.h"
 
 int main() {
-    printf("Hello, World! This is a native C program compiled on the command line.\n");
-    return 0;
+  fftw_complex a = {0.1, 2.3};
+  printf("Hello, World! This is a native C program compiled on the command line.\n");
+  return 0;
 }
