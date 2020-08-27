f9893f5c23107c2af2d1a2341f9d9202cf6f9b0b
Michael Boyle <michael.oliver.boyle@gmail.com>
2020-08-27 10:30:07 -0400

    Declare array of fftw_complex differently

--- simple.c
+++ simple.c
@@ -7,7 +7,9 @@ int main() {
   int lmax = 8;
   int Nm = 2*lmax+1;
   fftw_complex a = {0.1, 2.3};
-  fftw_complex *c = fftw_malloc(Nm*sizeof(fftw_complex));
+  /* fftw_complex *c = fftw_malloc(Nm*sizeof(fftw_complex)); */
+  /* fftw_complex c[Nm] = fftw_malloc(Nm*sizeof(fftw_complex)); */
+  fftw_complex c[Nm];
 
   for (m=0; m<Nm; m++) {
     /*************************************/
@@ -46,16 +48,19 @@ int main() {
     /*************************************/
     /* complex.h                         */
     /*************************************/
-    /* c[m] = {0.0, 0.0};  /\* error C2059: syntax error: '{' *\/ */
+    c[m] = {0.0, 0.0};  /* error C2059: syntax error: '{' */
 
-    /* c[m][0] = 0.0;  /\* works *\/ */
-    /* c[m][1] = 0.0; */
+    c[m][0] = 0.0;  /* works */
+    c[m][1] = 0.0;
 
-    /* c[m] = (fftw_complex){0.0, 0.0};  /\* error C2106: '=': left operand must be l-value *\/ */
+    c[m] = (fftw_complex){0.0, 0.0};  /* error C2106: '=': left operand must be l-value */
 
-    c[m] = _Dcomplex{0.0, 0.0};
+    c[m] = _Dcomplex{0.0, 0.0};  /* error C2275: '_Dcomplex': illegal use of this type as an expression
+        C:\Program Files (x86)\Windows Kits\10\include\10.0.19041.0\ucrt\complex.h(47): note: see declaration of '_Dcomplex'
+        simple.c(56): error C2143: syntax error: missing ';' before '{'
+        simple.c(56): error C2143: syntax error: missing ';' before '}' */
     
-    /* c[m] = 1.2 + 3.4 * I; */  /* error C2088: '*': illegal for struct */
+    c[m] = 1.2 + 3.4 * I;  /* error C2088: '*': illegal for struct */
   }
 
   return 0;
