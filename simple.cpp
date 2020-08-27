e8c55398f1a3579f15284f53a16aec13d96685be
Michael Boyle <michael.oliver.boyle@gmail.com>
2020-08-27 10:43:19 -0400

    Explicit cast for c++

--- simple.cpp
+++ simple.cpp
@@ -7,7 +7,7 @@ int main() {
   int lmax = 8;
   int Nm = 2*lmax+1;
   fftw_complex a = {0.1, 2.3};
-  fftw_complex *c = fftw_malloc(Nm*sizeof(fftw_complex));  /* error C2440: 'initializing': cannot convert from 'void *' to 'fftw_complex (*)' */
+  // fftw_complex *c = fftw_malloc(Nm*sizeof(fftw_complex));  /* error C2440: 'initializing': cannot convert from 'void *' to 'fftw_complex (*)' */
   fftw_complex *c = (fftw_complex *) fftw_malloc(Nm*sizeof(fftw_complex));
   /* fftw_complex c[Nm] = fftw_malloc(Nm*sizeof(fftw_complex)); */  /* error C2057: expected constant expression */
   /* fftw_complex c[Nm]; */  /* error C2057: expected constant expression */
@@ -15,10 +15,10 @@ int main() {
   for (m=0; m<Nm; m++) {
     c[m] = {0.0, 0.0};  /* error C2059: syntax error: '{' */
 
+    c[m] = (fftw_complex){0.0, 0.0};  /* error C2106: '=': left operand must be l-value */
+
     c[m][0] = 0.0;  /* works */
     c[m][1] = 0.0;
-
-    c[m] = (fftw_complex){0.0, 0.0};  /* error C2106: '=': left operand must be l-value */
 
     /* Obviously, this one errors out differently if complex.h is not included */
     c[m] = _Dcomplex{0.0, 0.0};  /* error C2275: '_Dcomplex': illegal use of this type as an expression
